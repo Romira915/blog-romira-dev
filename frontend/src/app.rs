c04e0d79c546ac48ad39d404447bc48da533d9ce
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-02-10 08:19:17 +0000

    refactoring

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -2,11 +2,14 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let hello = "hello";
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
             <h1>{ "Hello World!" }</h1>
+            <h2>{ hello }</h2>
             <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
         </main>
     }
 }
+
