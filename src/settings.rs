4de884117f6a37f8b9d8bc940146c6ccf3557314
Romira915 <40430090+Romira915@users.noreply.github.com>
2023-04-21 11:45:31 +0000

    fix: rustc 1.69よりtracing_subscriberにて正規表現エラーが出る問題の暫定対応

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use fern::colors::{Color, ColoredLevelConfig};
 use once_cell::sync::Lazy;
 use serde::Deserialize;
 
@@ -26,7 +27,7 @@ impl Config {
         &&self.newt_api_token
     }
 
-    pub fn rust_log_to_level_filter(&self) -> log::LevelFilter {
+    pub fn rust_log_to_log_level_filter(&self) -> log::LevelFilter {
         match self.rust_log.to_lowercase().as_str() {
             "error" => log::LevelFilter::Error,
             "warn" => log::LevelFilter::Warn,
@@ -43,53 +44,53 @@ pub static CONFIG: Lazy<Config> = Lazy::new(|| {
     envy::from_env().expect("Failed to env to Config.")
 });
 
-// pub fn init_logger(
-//     log_level: log::LevelFilter,
-//     log_file_path: Option<PathBuf>,
-// ) -> Result<(), fern::InitError> {
-//     let colors = ColoredLevelConfig::new()
-//         .info(Color::Green)
-//         .trace(Color::BrightBlack);
+pub fn init_logger(
+    log_level: log::LevelFilter,
+    log_file_path: Option<PathBuf>,
+) -> Result<(), fern::InitError> {
+    let colors = ColoredLevelConfig::new()
+        .info(Color::Green)
+        .trace(Color::BrightBlack);
 
-//     let base_config = fern::Dispatch::new()
-//         .level(log_level)
-//         .level_for("axum", log::LevelFilter::Info)
-//         .level_for("hyper", log::LevelFilter::Info)
-//         .level_for("tower-http", log::LevelFilter::Info)
-//         .level_for("tower", log::LevelFilter::Info);
+    let base_config = fern::Dispatch::new()
+        .level(log_level)
+        .level_for("axum", log::LevelFilter::Info)
+        .level_for("hyper", log::LevelFilter::Info)
+        .level_for("tower-http", log::LevelFilter::Info)
+        .level_for("tower", log::LevelFilter::Info);
 
-//     let stdout_config = fern::Dispatch::new()
-//         .format(move |out, message, record| {
-//             out.finish(format_args!(
-//                 "{}[{}][{}] {}",
-//                 chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
-//                 record.target(),
-//                 colors.color(record.level()),
-//                 message
-//             ))
-//         })
-//         .chain(std::io::stdout());
+    let stdout_config = fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+                record.target(),
+                colors.color(record.level()),
+                message
+            ))
+        })
+        .chain(std::io::stdout());
 
-//     let file_config = if let Some(log_file_path) = log_file_path {
-//         fern::Dispatch::new()
-//             .format(|out, message, record| {
-//                 out.finish(format_args!(
-//                     "{}[{}][{}] {}",
-//                     chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
-//                     record.target(),
-//                     record.level(),
-//                     message
-//                 ))
-//             })
-//             .chain(fern::log_file(log_file_path)?)
-//     } else {
-//         fern::Dispatch::new()
-//     };
+    let file_config = if let Some(log_file_path) = log_file_path {
+        fern::Dispatch::new()
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "{}[{}][{}] {}",
+                    chrono::Local::now().format("[%Y-%m-%d %H:%M:%S]"),
+                    record.target(),
+                    record.level(),
+                    message
+                ))
+            })
+            .chain(fern::log_file(log_file_path)?)
+    } else {
+        fern::Dispatch::new()
+    };
 
-//     base_config
-//         .chain(stdout_config)
-//         .chain(file_config)
-//         .apply()?;
+    base_config
+        .chain(stdout_config)
+        .chain(file_config)
+        .apply()?;
 
-//     Ok(())
-// }
+    Ok(())
+}
